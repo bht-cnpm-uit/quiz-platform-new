1430676384f05a09f4adda8bf127ce36c93a9abb
Minh Chau <buitongminhchau01032002@gmail.com>
2023-02-23 16:12:25 +0700

    add katex

--- app/(default)/page.js
+++ app/(default)/page.js
@@ -1,9 +1,12 @@
 import Editor from './components/Editor';
+import 'katex/dist/katex.min.css';
+import { InlineMath } from 'react-katex';
 
 export default function Home() {
     return (
         <div className="mx-auto max-w-[720px] bg-white">
             <Editor />
+            <InlineMath math="\int_0^\infty x^2 dx" />
         </div>
     );
 }
