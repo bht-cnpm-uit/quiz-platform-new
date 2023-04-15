0439f3d23887dc14c522f24add4bfff6c1a7b45b
Minh Chau <buitongminhchau01032002@gmail.com>
2023-04-15 11:24:31 +0700

    🍱 add firebase configs to env file

--- configs/firebase.js
+++ configs/firebase.js
@@ -7,12 +7,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyChLnWnglK0yyeUqd3Z1k4DLsVl0690FCo',
-    authDomain: 'quiz-b4bb9.firebaseapp.com',
-    projectId: 'quiz-b4bb9',
-    storageBucket: 'quiz-b4bb9.appspot.com',
-    messagingSenderId: '738779623879',
-    appId: '1:738779623879:web:58c502eeb9473382b229e2',
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
